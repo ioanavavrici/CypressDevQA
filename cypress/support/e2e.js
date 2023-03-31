42ba2e8b94cb6840c1eebea7c2bd7070cc9d5d7c
Emy Iacob <emy.iacob@assist.ro>
2023-03-31 11:33:56 +0300

    Test example updated

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -12,18 +12,3 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-Cypress.Server.defaults({
-    ignore: xhr => true 
-     
- })
-
- Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-  })
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
